57ad781b71a0d10c3ff5ddd1b8d32dd7d5dce1ea
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2020-09-22 20:56:42 +0600

    Updates

---
+++ precache-manifest.699b964c12d2dbb035ac1afa3f831361.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd6723beed37391d6a4b2ce624ac6b1",
+    "url": "/map_maker/index.html"
+  },
+  {
+    "revision": "9d1cab5d6ec208d1b3fb",
+    "url": "/map_maker/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "d31ca0319ddebf77d894",
+    "url": "/map_maker/static/css/main.8c5ec0aa.chunk.css"
+  },
+  {
+    "revision": "9d1cab5d6ec208d1b3fb",
+    "url": "/map_maker/static/js/2.e6841bcf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/map_maker/static/js/2.e6841bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31ca0319ddebf77d894",
+    "url": "/map_maker/static/js/main.bd04ea63.chunk.js"
+  },
+  {
+    "revision": "2c541b268ebd81a5e7c6",
+    "url": "/map_maker/static/js/runtime-main.6866ed58.js"
+  },
+  {
+    "revision": "29cbe0d4e755a74ea88bc92b0c597486",
+    "url": "/map_maker/static/media/fall.29cbe0d4.png"
+  },
+  {
+    "revision": "f00a854a099ae572ef4f14a1e93dc830",
+    "url": "/map_maker/static/media/flower.f00a854a.png"
+  },
+  {
+    "revision": "4ad9d4a2daf95c131005d824aab1dfd6",
+    "url": "/map_maker/static/media/spring.4ad9d4a2.png"
+  },
+  {
+    "revision": "a1cf1d290aa6eacaea887ccf47392b45",
+    "url": "/map_maker/static/media/winter.a1cf1d29.png"
+  }
+]);
